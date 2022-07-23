56d3fd1354f13f946bab97ad9676afa9dae0300d
mugaira <mugairainamdar@gmail.com>
2022-07-23 13:02:14 +0530

    Step 3 completed. Create Header and Footer components done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Footer from './components/Footer'
 import Header from './components/Header'
 
 const App = () => {
   return (
+    <div>
     <Header />
+    <div style={{height : '72px'}}>
+
+    </div>
+    <Footer />
+    </div>
   )
 }
 
