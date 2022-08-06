26ae7a8dadbce21e741004bd8f01ca940c63708a
mugaira <mugairainamdar@gmail.com>
2022-08-06 18:46:34 +0530

    Step 12 and 13 completed. Connect to the database done.

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import products from './data/products.js';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config();
 
+connectDB();
 
 const app = express();
 
 app.get('/',(req,res)=>{
- res.send('api is running')
+ res.send('api is running');
 });
 
 app.get('/api/products',(req,res)=>{
- res.json(products)
+ res.json(products);
 })
 
 app.get('/api/products/:id',(req,res)=>{
@@ -23,5 +25,5 @@ app.get('/api/products/:id',(req,res)=>{
 const PORT = process.env.PORT || 5000;
 
 app.listen(process.env.PORT ,()=>{
- console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+ console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 })
