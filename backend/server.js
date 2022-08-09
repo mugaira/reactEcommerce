8616aabe775f5ad75b8b4a9074c8d6f5086037db
mugaira <mugairainamdar@gmail.com>
2022-08-09 12:10:45 +0530

    Step 18 and 19 completed. Fetching products from the database done.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import products from './data/products.js';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import colors from 'colors'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -10,21 +10,16 @@ connectDB();
 
 const app = express();
 
-app.get('/',(req,res)=>{
- res.send('api is running');
+app.get("/", (req, res) => {
+ res.send("API is running");
 });
 
-app.get('/api/products',(req,res)=>{
- res.json(products);
-})
-
-app.get('/api/products/:id',(req,res)=>{
- const product = products.find((product)=> product._id === req.params.id);
- res.json(product);
-})
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT ,()=>{
- console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold);
-})
+app.listen(process.env.PORT, () => {
+ console.log(
+  `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold
+ );
+});
