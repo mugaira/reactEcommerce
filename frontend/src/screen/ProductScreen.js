40943ad9b3940dfc8141125b380bfc2c1c9d5352
mugaira <mugairainamdar@gmail.com>
2022-08-17 18:41:05 +0530

    Step 25 completed. Single product details screen Reducer and Action done.

--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -1,22 +1,23 @@
-import { Link as RouterLink, useParams } from "react-router-dom";
 import { Button, Flex, Grid, Heading, Image, Text } from "@chakra-ui/react";
+import { Link as RouterLink, useParams } from "react-router-dom";
+import { listProductDetails } from "../actions/productActions";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import { useEffect } from "react";
 import Rating from "../components/Rating";
-import { useState,useEffect } from "react";
+import Loader from "../components/Loader";
 import React from "react";
-import axios from "axios";
 
 const ProductScreen = () => {
-	const [product,setProduct] = useState({})
+	const dispatch = useDispatch();
 	const { id } = useParams();
 
-	useEffect(()=>{
-		const fetchProduct = async() => {
-			const { data } = await axios.get(`/api/products/${id}`);
-			console.log(data)
-			setProduct(data);
-				}
-				fetchProduct();
-	},[id])
+	const productDetails = useSelector((state) => state.productDetails);
+	const { loading, error, product } = productDetails;
+
+	useEffect(() => {
+		dispatch(listProductDetails(id));
+	}, [id, dispatch]);
 
 	return (
 		<>
@@ -26,62 +27,71 @@ const ProductScreen = () => {
 				</Button>
 			</Flex>
 
-			<Grid templateColumns={ { base:"1fr", md:"1fr 1fr", lg:"5fr 4fr 3fr"}  }gap="10">
-				{/* Coloum 1 */}
-				<Image src={product.image} alt={product.name} mb="10" />
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message type="error">{error}</Message>
+			) : (
+				<Grid
+					templateColumns={{ base: "1fr", md: "1fr 1fr", lg: "5fr 4fr 3fr" }}
+					gap="10"
+				>
+					{/* Coloum 1 */}
+					<Image src={product.image} alt={product.name} mb="10" />
 
-				{/* coloum 2 */}
-				<Flex direction="column">
-					<Heading
-						as="h6"
-						textTransform="uppercase"
-						fontWeight="bold"
-						fontSize="sm"
-						color="gray"
-					>
-						{" "}
-						{product.brand}{" "}
-					</Heading>
-					<Heading
-						as="h2"
-						textTransform={"uppercase"}
-						fontWeight="bold"
-						fontSize="lg"
-					>
-						{product.name}
-					</Heading>
+					{/* coloum 2 */}
+					<Flex direction="column">
+						<Heading
+							as="h6"
+							textTransform="uppercase"
+							fontWeight="bold"
+							fontSize="sm"
+							color="gray"
+						>
+							{" "}
+							{product.brand}{" "}
+						</Heading>
+						<Heading
+							as="h2"
+							textTransform={"uppercase"}
+							fontWeight="bold"
+							fontSize="lg"
+						>
+							{product.name}
+						</Heading>
 
-					<Rating value={product.rating} text={product.numReviews} />
-					<Heading as="h5" fontSize="xl" color="teal.600">
-						₹{product.price}
-					</Heading>
-					<Text>{product.description}</Text>
-				</Flex>
-
-				{/* column 3 */}
-				<Flex direction="column">
-					<Flex justifyContent="space-between" py="2">
-						<Text>Price:</Text>
-						<Text fontWeight="bold">₹{product.price}</Text>
+						<Rating value={product.rating} text={product.numReviews} />
+						<Heading as="h5" fontSize="xl" color="teal.600">
+							₹{product.price}
+						</Heading>
+						<Text>{product.description}</Text>
 					</Flex>
-					<Flex justifyContent="space-between" py="1">
-						<Text>Status:</Text>
-						<Text fontWeight="bold">
-							{product.countInStock > 0 ? "InStock" : "Not available'"}
-						</Text>
+
+					{/* column 3 */}
+					<Flex direction="column">
+						<Flex justifyContent="space-between" py="2">
+							<Text>Price:</Text>
+							<Text fontWeight="bold">₹{product.price}</Text>
+						</Flex>
+						<Flex justifyContent="space-between" py="1">
+							<Text>Status:</Text>
+							<Text fontWeight="bold">
+								{product.countInStock > 0 ? "InStock" : "Not available'"}
+							</Text>
+						</Flex>
+						<Button
+							bgColor="gray.800"
+							textTransform="uppercase"
+							letterSpacing="wide"
+							my="2"
+							colorScheme="red"
+							disabled={product.countInStock === 0}
+						>
+							Add To Cart
+						</Button>
 					</Flex>
-					<Button
-						bgColor="gray.800"
-						textTransform="uppercase"
-						letterSpacing="wide"
-						my="2"
-						colorScheme="red"
-						disabled={product.countInStock === 0}
-					>
-						Add To Cart
-					</Button>
-				</Flex>
-			</Grid>
+				</Grid>
+			)}
 		</>
 	);
 };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,20 +1,24 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import { productListReducer } from './reducers/productReducer'
+import {
+ productDetailsReducer,
+ productListReducer,
+} from "./reducers/productReducer";
 
 const reducer = combineReducers({
- productList : productListReducer,
+ productList: productListReducer,
+ productDetails: productDetailsReducer,
 });
 
 const initialState = {};
 
-const middleware = [thunk];
+const middlewares = [thunk];
 
 const store = createStore(
  reducer,
  initialState,
- composeWithDevTools(applyMiddleware(...middleware))
+ composeWithDevTools(applyMiddleware(...middlewares))
 );
 
 export default store;
