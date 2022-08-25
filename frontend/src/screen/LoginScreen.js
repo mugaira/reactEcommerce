9537622b44bb7a993dda3619d5554de33a494576
mugaira <mugairainamdar@gmail.com>
2022-08-25 11:12:54 +0530

    step 37 completed. Implement all the Login – Redux functionality in the LoginScreen component. done

--- frontend/src/screen/LoginScreen.js
+++ frontend/src/screen/LoginScreen.js
@@ -1,15 +1,49 @@
-import { Button, Flex, FormControl, FormLabel, Heading, Input, Link, Spacer, Text } from "@chakra-ui/react";
+import {
+	Button,
+	Flex,
+	FormControl,
+	FormLabel,
+	Heading,
+	Input,
+	Link,
+	Spacer,
+	Text,
+} from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
 	Link as RouterLink,
+	useNavigate,
+	useSearchParams,
 } from "react-router-dom";
+import { login } from "../actions/userActions";
 import FormContainer from "../components/FormContainer";
+import Message from "../components/Message";
 import React from "react";
 
 const LoginScreen = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-	const submitHandler = () => {
+	const [searchParams] = useSearchParams();
+	const redirect = searchParams.get("redirect") || "/";
 
-	}
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const userLogin = useSelector((state) => state.userLogin);
+	const { loading, error, userInfo } = userLogin;
+
+	useEffect(() => {
+		if (userInfo) {
+			navigate(redirect);
+		}
+	}, [navigate, userInfo, redirect]);
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		dispatch(login(email, password));
+	};
 
 	return (
 		<Flex w="full" alignItems="center" justifyContent="center" py="5">
@@ -18,41 +52,44 @@ const LoginScreen = () => {
 					Login
 				</Heading>
 
+				{error && <Message type="error">{error}</Message>}
+
 				<form onSubmit={submitHandler}>
 					<FormControl id="email">
-						<FormLabel >Email address</FormLabel>
+						<FormLabel>Email address</FormLabel>
 						<Input
-							id='email'
-							type='email'
-							placeholder='username@domain.com'
+							id="email"
+							type="email"
+							placeholder="username@domain.com"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
 						/>
 					</FormControl>
 
-					<Spacer h='3' />
+					<Spacer h="3" />
 
-					<FormControl id='password'>
+					<FormControl id="password">
 						<FormLabel>Password</FormLabel>
 						<Input
-							id='password'
-							type='password'
-							placeholder='*********'
+							id="password"
+							type="password"
+							placeholder="*********"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
 						/>
-
 					</FormControl>
 
-					<Button type='submit' colorScheme='teal' mt='4'>
+					<Button type="submit" colorScheme="teal" mt="4" isLoading={loading}>
 						Login
 					</Button>
-
 				</form>
 
-				<Flex pt='10'>
-					<Text fontWeight='semibold' >
-						New Customer?{' '}
-						<Link as={RouterLink} to='/register'>
+				<Flex pt="10">
+					<Text fontWeight="semibold">
+						New Customer?{" "}
+						<Link as={RouterLink} to="/register">
 							Click here to register
 						</Link>
-
 					</Text>
 				</Flex>
 			</FormContainer>
