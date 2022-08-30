eb4ed258ceebaf5c44201710597e136f045689c8
mugaira <mugairainamdar@gmail.com>
2022-08-30 11:38:27 +0530

    Step 40 completed. Update User Profile endpoint in the backend done.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
-import asyncHandler from "express-async-handler";
-import User from "../models/userModel.js";
-import generateToken from "../utils/generateToken.js";
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc   Auth user & get token
 // @route  POST /api/user/login
@@ -21,7 +21,7 @@ const authUser = asyncHandler(async (req, res) => {
   });
  } else {
   res.status(401); //Unauthorized
-  throw new Error("Invalid email or password");
+  throw new Error('Invalid email or password');
  }
 });
 
@@ -41,7 +41,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
   });
  } else {
   res.status(404);
-  throw new Error("User Not Found");
+  throw new Error('User Not Found');
  }
 });
 
@@ -56,7 +56,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
  if (userExists) {
   res.status(400); //Bad rrequest
-  throw new Error("user/email already exist");
+  throw new Error('user/email already exist');
  }
 
  const user = await User.create({ name, email, password });
@@ -72,4 +72,33 @@ const registerUser = asyncHandler(async (req, res) => {
  }
 });
 
-export { authUser, getUserProfile, registerUser };
+// @desc   Update user profile
+// @route  put /api/users/profile
+// @access private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+ const user = await User.findById(req.user._id);
+
+ if (user) {
+  user.name = req.body.name || user.name;
+  user.email = req.body.email || user.email;
+  if (req.body.password) {
+   user.password = req.body.password;
+  }
+
+  const updateUser = await user.save();
+
+  res.json({
+   _id: updateUser._id,
+   name: updateUser.name,
+   email: updateUser.email,
+   isAdmin: updateUser.isAdmin,
+   token: generateToken(updateUser._id),
+  });
+ } else {
+  res.status(404);
+  throw new Error('User Not Found');
+ }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
