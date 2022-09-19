49144a557e8d18f562b413bc7433cc7c7c738787
mugaira <mugairainamdar@gmail.com>
2022-09-19 12:37:48 +0530

    Step 70 completed. Image Upload Configuration and Endpoint done.

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+ destination(req, file, cd) {
+  cb(null, 'uploads');
+ },
+ filename(req, file, cb) {
+  cb(
+   null,
+   `${file.fieldname}-${Date.now()}.${path.extname(file.originalname)}`
+  );
+ },
+});
+
+function checkFileType(file, cb) {
+ const filetype = /jpeg|jpg|png/;
+ const extname = filetype.test(path.extname(file.originalname).toLowerCase());
+ const mimetype = filetype.test(file.mimetype);
+
+ if (extname && mimetype) {
+  cb(null, true);
+ } else {
+  cb('Only image are allowed');
+ }
+}
+
+const upload = multer({
+ storage,
+ fileFilter: function (req, file, cb) {
+  checkFileType(file, cb);
+ },
+});
+
+router.post('/', upload.single('image'), (req, res) => {
+ res.json(`${req.file.path}`);
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/uploads',uploadRoutes);
 
 //Error Middleware
 
