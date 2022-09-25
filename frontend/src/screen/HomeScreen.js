24b19b4bdea87a960da0e1f64cf1297c40f5cc7d
mugaira <mugairainamdar@gmail.com>
2022-09-25 08:01:49 +0530

    Carousel and Banner Added

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,52 +1,64 @@
-import React from "react";
-import { Heading, Grid } from "@chakra-ui/react";
-import { useEffect } from "react";
-import Product from "../components/Product";
-import { useDispatch, useSelector } from "react-redux";
-import { listProducts } from "../actions/productActions";
-import Loader from "../components/Loader";
-import Message from "../components/Message";
+import React from 'react';
+import { Heading, Grid } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import Product from '../components/Product';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import Carousel from '../components/Carousel';
+import HomeBannerOne from '../components/BannerOne';
 
 const HomeScreen = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  // const products = [1,2,3];
+	// const products = [1,2,3];
 
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+	const productList = useSelector((state) => state.productList);
+	const { loading, error, products } = productList;
 
-  useEffect(() => {
-    // console.log("i reached here");
-    dispatch(listProducts());
-  }, [dispatch]);
+	useEffect(() => {
+		// console.log("i reached here");
+		dispatch(listProducts());
+	}, [dispatch]);
 
-  return (
-    <div>
-      <Heading as="h2" fontSize="3xl" mb="8">
-        Latest Product
-      </Heading>
+	return (
+		<div>
+			<Carousel />
+			<HomeBannerOne />
 
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message type="error" >{error}</Message>
-      ) : (
-        <Grid
-          templateColumns={{
-            sm: "1fr",
-            md: "1fr 1fr",
-            lg: "1fr 1fr 1fr",
-            xl: "1fr 1fr 1fr 1fr",
-          }}
-          gap="8"
-        >
-          {products.map((prod) => (
-            <Product key={prod._id} product={prod} />
-          ))}
-        </Grid>
-      )}
-    </div>
-  );
+			<Heading
+				as='h2'
+				fontSize='3xl'
+				mb='8'
+			>
+				Feature Product
+			</Heading>
+
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message type='error'>{error}</Message>
+			) : (
+				<Grid
+					templateColumns={{
+						sm: '1fr',
+						md: '1fr 1fr',
+						lg: '1fr 1fr 1fr',
+						xl: '1fr 1fr 1fr 1fr',
+					}}
+					gap='8'
+				>
+					{products.map((prod) => (
+						<Product
+							key={prod._id}
+							product={prod}
+						/>
+					))}
+				</Grid>
+			)}
+		</div>
+	);
 };
 
 export default HomeScreen;
