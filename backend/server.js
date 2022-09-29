46c00ef24b84f4df0bd8a1f0bc933fef56acf288
mugaira <mugairainamdar@gmail.com>
2022-09-29 12:10:34 +0530

    Ready for production

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,46 @@
-import colors from "colors";
+import colors from 'colors';
 import path from 'path';
-import dotenv from "dotenv";
-import express from "express";
-import connectDB from "./config/db.js";
+import dotenv from 'dotenv';
+import express from 'express';
+import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
-import productRoutes from "./routes/productRoutes.js";
-import userRoutes from './routes/userRoutes.js'
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 
-
 connectDB();
 
 const app = express();
 
-app.use(express.json()); //body parsing 
+app.use(express.json()); //body parsing
 
-app.get("/", (req, res) => {
- res.send("API is running");
-});
-
-app.use("/api/products", productRoutes);
+app.use('/api/products', productRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/orders', orderRoutes);
-app.use('/api/uploads',uploadRoutes);
+app.use('/api/uploads', uploadRoutes);
 
 //create static folder
 const __dirname = path.resolve();
-app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+ app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+ app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+ });
+} else {
+ app.get('/', (req, res) => {
+  res.send('API is running');
+ });
+}
 
 //Error Middleware
 app.use(notFound);
 app.use(errorHandler);
-
 
 const PORT = process.env.PORT || 5000;
 
